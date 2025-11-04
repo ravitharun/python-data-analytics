0ef8e1150bbb7c8f82e8b42dac320076aa4fd9fd
Ravi Tharun <tr565003@gmail.com>
2025-11-04 07:32:51 +0530

    Learned how to convert and handle data into specific data types

--- class1.py
+++ class1.py
@@ -1,1 +1,7 @@
 print("hello wold ")
+Studentname="ravi tharun"
+StudentUsn="eng22cs05758"
+StudentSection="3A"
+print(Studentname,'\n',StudentUsn,'\n',StudentSection)
+userinput=10
+print(float(userinput))
