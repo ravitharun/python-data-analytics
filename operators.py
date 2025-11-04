dd86519581674ed7bb5ba19ccee8a4c407c861e4
Ravi Tharun <tr565003@gmail.com>
2025-11-04 08:47:05 +0530

    Understood bitwise operators (AND, OR, XOR, shift) and binary value conversions

---
+++ operators.py
@@ -0,0 +1,21 @@
+a=10
+b=8
+# print(bin(a &b))
+c=a&b
+d=a or b
+e=a ^ b
+
+print("binary value of a",bin(a))
+print("binary value of b",bin(b))
+print("binary value of c",bin(c))
+print(f'value of c = {c}')
+print(f'value of d or operator  = {d}')
+print(f'value of d exor operator  = {e}')
+print(f'the left shift of a and b {a>>b}')
+print(f'the right shift of a and b {a<<b}')
+
+
+
+
+
+
