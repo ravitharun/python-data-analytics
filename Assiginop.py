66699e36ba8f75e6101c85ec749ae17ccb80645a
Ravi Tharun <tr565003@gmail.com>
2025-11-04 11:35:09 +0530

    understood how to use if, else, and nested if statements in Python

---
+++ Assiginop.py
@@ -0,0 +1,4 @@
+score=10
+Mscore=10
+print(score)
+print(Mscore is not score)
---
+++ condotionst.py
@@ -0,0 +1,22 @@
+# score=int(input("enter your total marks : "))
+# if score>=90:
+#     print("grade=A")
+# elif score>=45:
+#     print("Grade =pass")   
+# else:
+#     print("grade = fail")   
+
+
+
+# nested if condition 
+Checknumber = int(input("Enter an integer value: "))
+
+if Checknumber > 0:
+    if Checknumber % 2 == 0:
+        print("Positive and even number")
+    else:
+        print("Positive and odd number")
+elif Checknumber is 0:
+    print("Zero")
+else:
+    print("Negative number")
