051bad98b79299702607f954a5ca6b2d5963bb50
Ravi Tharun <tr565003@gmail.com>
2025-11-04 14:54:58 +0530

    got understand the loops in py

---
+++ Loops.py
@@ -0,0 +1,14 @@
+# for i in range (1,6):
+#     print(f'{i} hello wolrd')
+
+
+# table=int(input('enter the tabel number you want : '))
+# for i in range (1,11):
+#     print(f"{table} * {i} = {table*i}")
+
+# while loop
+value=0
+while(value<=10):
+    print(value)
+    value+=3
+print(value)    
