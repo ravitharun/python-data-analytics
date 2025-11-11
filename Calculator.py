f33ca83f33f1f7b16d62490ff202ced43940b9ce
Ravi Tharun <tr565003@gmail.com>
2025-11-11 08:26:00 +0530

    A simple calculator in py

---
+++ Calculator.py
@@ -0,0 +1,9 @@
+# Simple calculator 
+while True:
+    print(eval(input("enter the vale 5+122.0 :")))
+    userinp=input("do u want to exits yes/no")    
+    if(userinp=='yes'):
+         break
+    else:
+        continue
+    
