ddeb0897f108e7451739eb0ace9ef9734d81df38
Ravi Tharun <tr565003@gmail.com>
2025-11-15 09:20:21 +0530

    nested dict

---
+++ nestedDict.py
@@ -0,0 +1,34 @@
+student = {
+    "name": "RAVITHARUN",
+    "details": {
+        "age": 21,
+        "city": "Bangalore",
+        "course": "CSE"
+    }
+}
+
+# print(student)
+# print(student["name"])
+# print(student["details"]['age'])
+# student["details"]['age']=25
+# print(student)
+# print(student.keys())
+# print(student.values())
+# student.update({
+    
+#     'name':"thij",
+  
+# })
+
+student['marks']={
+    
+    "maths":56,
+    "Physice":96,
+}
+for i in student:
+    # print()
+    if student[i]=={'maths': 56, 'Physice': 96} :
+        student[i]='tharunkumar'
+        student[i]={'maths': 56, 'Physice': 96,"chemistry":24,"totla":456}
+
+print(student)    
