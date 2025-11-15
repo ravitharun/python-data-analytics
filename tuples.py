f4cc90298b760e47ab0f261abc3cda111a7e5c83
Ravi Tharun <tr565003@gmail.com>
2025-11-15 08:27:50 +0530

    tuples methods in py

--- tuples.py
+++ tuples.py
@@ -2,3 +2,18 @@
 tuples=(1,2,"tharun")
 
 print(tuples[0])
+print(tuples.count(2))
+print(tuples.index("tharun"))
+t = (3, 5, 9)
+print(max(t))   # Output: 9
+print(min(t))   # Output: 3
+print(sum(t))   # Output: 17
+print(sorted(t))   # Output: [3,5,9]
+
+t = 10, 20, 30
+a, b, c = (10, 20, 30)
+print(f'a={a} b={b} c={c}')
+tuples=(1,2,"tharun")
+print([tuples])
+list_itm=[1,2,3,4]
+print(tuple(list_itm))
